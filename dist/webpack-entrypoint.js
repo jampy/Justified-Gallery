82b66e1ead3c30299c609fe3e50ae85965162e0d
Udo Giacomozzi <udo.giacomozzi@indunet.it>
2016-02-27 12:18:28 +0100

    fix Webpack layer

--- dist/webpack-entrypoint.js
+++ dist/webpack-entrypoint.js
@@ -1,2 +1,3 @@
-require("./css/justifiedGallery.css");
-module.exports = require("./js/jquery.justifiedGallery.js");
+require("css-loader!./css/justifiedGallery.css");
+module.exports = require("imports?jQuery=jquery!./js/jquery.justifiedGallery.js")
+
